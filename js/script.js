6e664475b316d6bde54e49079d40f7675fc841cf
carolopez <caroolinalopez4@gmail.com>
2023-02-20 17:18:31 -0300

    script

--- js/script.js
+++ js/script.js
@@ -3,81 +3,81 @@
 2-Si elige uno que queda seleccionado con su precio 
 3-Si elige más productos que se sumen , se multiplica si elige mas de una unidad del producto(máximo 5)
 4-Precio final*/
-let precioBuzoMarron = 14.000;
-let precioBuzoGris = 16.150;
-let precioBuzoRosado = 11.640;
-let precioRemeraBlanca = 7.050;
+let precioBuzoMarron = 14000;
+let precioBuzoGris = 16150;
+let precioBuzoRosado = 11640;
+let totalProductos = 0;
 
-let elegirProducto = prompt("Elige un producto: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Remera Blanca. \n5- Presiona N para abandonar.");
+function saludarConNombre(nombre) {
+    alert('Buenos días, ' + nombre);
+}
 
-while (elegirProducto != "N" && elegirProducto != "n") {
-    switch (elegirProducto) {
-        case "1":
-            alert("Tu saldo es $ " + precioBuzoMarron);
-            let unidad;
-            for (let i = 0; i <= 5; i++) {
-                unidad = prompt("¿Desea más de una unidad \n1- Si. n2\-No.");
-                if (unidad != "No" && unidad != "no") {
-                    elegirProducto += unidad;
-                    alert("Se sumo a tu compra:" + unidad + " unidad/es");
-                    elegirProducto = precioBuzoMarron * unidad;
-                    alert("Su precio total sería" + elegirProducto);
-                    break;
+let elegirProducto = prompt("¿Desea comprar?");
+
+    if (elegirProducto === "si" || elegirProducto === "Si") {
+
+    let producto = prompt("Elige un producto: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Presiona N para abandonar.");
+
+    while (elegirProducto != "N" && elegirProducto != "n") {
+        switch (producto) {
+            case "1":
+                alert("El precio es de $ " + precioBuzoMarron);
+                    totalProductos = prompt("¿Desea más de una unidad: \n1- Si. \n2- No.");
+                    if (totalProductos != "No" && totalProductos != "no") {
+                        for (let index = 0; index <= 5; index++) {
+                            unidad = 0;
+                            let unidad =parseInt(prompt("¿Cuánto unidades? Se tiene como límite 5 unidades de cada producto"));
+                            alert(unidad);
+                            precioBuzoMarron= (unidad * precioBuzoMarron );
+                            alert("Su precio total sería" + precioBuzoMarron);}
+                
+                    }else {
+                    alert("Gracias por comprar");
                 }
-            alert("Gracias por comprar")
-                
-            }
-        case "2":
-            alert("Tu saldo es $ " + precioBuzoGris);
-            let unidad1;
-            for (let i = 0; i <= 5; i++) {
-                unidad1 = prompt("¿Desea más de una unidad \n1- Si. n2\-No.");
-                if (unidad1 != "No" && unidad1 != "no") {
-                    elegirProducto += unidad1;
-                    alert("Se sumo a tu compra:" + unidad1 + " unidad/es");
-                    elegirProducto = precioBuzoGris * unidad1;
-                    alert("Su precio total sería" + elegirProducto);
                     break;
-            }
-            alert("Gracias por comprar")
+
                 
-            }
-        case "3":
-            alert("Tu saldo es $ " + precioBuzoGris);
-            let unidad2;
-            for (let i = 0; i <= 5; i++) {
-                unidad3 = prompt("¿Desea más de una unidad \n1- Si. n2\-No.");
-                if (unidad2 != "No" && unidad3 != "no") {
-                    elegirProducto += unidad2;
-                    alert("Se sumo a tu compra:" + unidad2 + " unidad/es");
-                    elegirProducto = precioBuzoGris * unidad2;
-                    alert("Su precio total sería" + elegirProducto);
+            case "2":
+                alert("El precio es de $ " + precioBuzoGris);
+                    totalProductos = prompt("¿Desea más de una unidad: \n1- Si. \n2- No.");
+                    if (totalProductos != "No" && totalProductos != "no") {
+                        for (let index = 0; index <= 5; index++) {
+                            unidad = 0;
+                            let unidad =parseInt(prompt("¿Cuánto unidades? Se tiene como límite 5 unidades de cada producto"));
+                            alert(unidad);
+                            precioBuzoGris= (unidad * precioBuzoGris );
+                            alert("Su precio total sería" + precioBuzoGris);}
+                    }else{
+                    alert("Gracias por comprar");
+                    }
                     break;
-            }
-            alert("Gracias por comprar")
                 
-        }
-        case "4":
-            alert("Tu saldo es $ " + precioBuzoGris);
-            let unidad3;
-            for (let i = 0; i <= 5; i++) {
-                unidad3 = prompt("¿Desea más de una unidad \n1- Si. n2\-No.");
-                if (unidad3 != "No" && unidad3 != "no") {
-                    elegirProducto += unidad3;
-                    alert("Se sumo a tu compra:" + unidad3 + " unidad/es");
-                    elegirProducto = precioBuzoGris * unidad3;
-                    alert("Su precio total sería" + elegirProducto);
+            case "3":
+                alert("El precio es de $ " + precioBuzoRosado);
+                    totalProductos = prompt("¿Desea más de una unidad: \n1- Si. \n2- No.");
+                    if (totalProductos != "No" && totalProductos != "no") {
+                        for (let index = 0; index <= 5; index++) {
+                            unidad = 0;
+                            let unidad =parseInt(prompt("¿Cuánto unidades? Se tiene como límite 5 unidades de cada producto"));
+                            alert(unidad);
+                            precioBuzoRosado= (unidad * precioBuzoRosado );
+                            alert("Su precio total sería" + precioBuzoRosado)}
+                
+                    }else {
+                    alert("Gracias por comprar")
+                    }
                     break;
-            }
-            alert("Gracias por comprar")
                 
+            default:
+                alert("Gracias por el tiempo. Te esperamos");
+                break;
+        
         }
             
-        default:
-            alert("Gracias por el tiempo. Te esperamos");
-            break;
+        producto = prompt("Elige un producto: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Presiona N para abandonar.");
     }
-
-    
-
-let elegirProducto = prompt("Elige un producto: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Remera Blanca. \n5- Presiona N para abandonar.");}
+}else {
+        
+        alert("Gracias por el tiempo. Te esperamos");
+    }
+alert("Adiós");
