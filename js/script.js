e91e2c8196991dbfaf6b6f59aa1aca392dd5476e
carolopez <caroolinalopez4@gmail.com>
2023-03-11 12:39:38 -0300

    agregue script

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-/* carrito de compras
-1-Elegir uno o más productos
-2-Si elige uno que queda seleccionado con su precio 
-3-Si elige más productos que se sumen , se multiplica si elige mas de una unidad del producto(máximo 5)
-4-Precio final*/
-let precioBuzoMarron = 14000;
-let precioBuzoGris = 16150;
-let precioBuzoRosado = 11640;
-let totalProductos = 0;
+//--------------------- poner todos los productos en un objeto class constructor----------------------//
 unidad = 0;
+class listaRopa{
 
+    constructor(buzoMarron, buzoGris, buzoRosado){
+
+    this.buzoMarron = parseInt(buzoMarron);
+    this.buzoGris= parseInt(buzoGris);
+    this.buzoRosado= parseInt(buzoRosado);
+    this.vendido = false;
+}
+    sumaIva(){
+    this.precio = this.precio * 1.21;
+}
+    vender(){
+    this.vendido = true;
+}
+}
+listaRopa.sumaIva();
+listaRopa.vender();
+//---------------------guardar los precios de la ropa en un arrays------------------------------------//
+
+const listasRopa = [
+    new listaRopa (15750,17230,13230),
+    new listaRopa (14000,16150,11640),
+]
+
+console.log(listaRopa);
+
+
+
+//--------------------- pedir al usuario su nombre y preguntar si desea comprar ---------------------//
 
 let nombre = prompt("Ingrese su nombre , por favor");
 
@@ -23,7 +44,10 @@ if (elegirProducto === "si" || elegirProducto === "Si") {
 
     let producto = prompt("Elige un producto: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Presiona N para abandonar.");
 
+    //-----preguntar al usuario qué desea comprar y sumar otra unidad en que caso de que lo desee---------//
+
     while (elegirProducto != "N" && elegirProducto != "n") {
+
         switch (producto) {
             case "1":
                 alert("El precio es de $ " + precioBuzoMarron);
@@ -94,4 +118,16 @@ if (elegirProducto === "si" || elegirProducto === "Si") {
 } else {
     alert("Adiós, gracias por tu tiempo!");
 }
+//----------------------------- pedir al usuario el método de pago-----------------------------------------//
 
+let elegirIngreso = prompt("Los métodos de pago: n1\- Tarjeta de crédito. n2\-Tarjeta de débito n3\-Mercado Pago. n4\-X Para cancelar el pago")
+
+while (elegirIngreso != "x" && elegirIngreso!= "X") {
+
+    switch (elegirIngreso) {
+        case "1": alert( "Elegió +  " + elegirIngreso)
+    listaRopa.push(elegirIngreso);
+
+    console.log(elegirIngreso)
+} 
+}
