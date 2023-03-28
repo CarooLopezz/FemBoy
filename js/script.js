1f853723cff8b6a6af42f4c0243ba1b0599a6036
carolopez <caroolinalopez4@gmail.com>
2023-03-28 00:16:37 -0300

    ultimo

--- js/script.js
+++ js/script.js
@@ -1,87 +1,128 @@
-//--------------------- creo las clavas para el localstorage----------------------------------------//
-localStorage.setItem(1, "Buzo ", "L", "marrón", 14000);
-localStorage.setItem(2, "Buzo", "M", "gris", 16150);
-sessionStorage.setItem(3, "Buzo", "X", "rosado", 11640);
-sessionStorage.setItem(4, "Remera", "S", "blanca", 7050);
+//------------creo la clase prenda, con las propiedades id, nombre, talle ,color y precio-------------//
 
-
-//--------------------- todos los elementos del DOM que voy a necesitar------------------------------//
-const btncarrito1 = document.getElementById("carrito1");
-const btncarrito2 = document.getElementById("carrito2");
-const btncarrito3 = document.getElementById("carrito3");
-const btncarrito4 = document.getElementById("carrito4");
-
-//-------------- --para iniciar el carrito ---------------------------------------------------------------//
-btncarrito.onclick=(evento) => {
-    
-    console.log('Se agregó exitosamente')
+class Prenda { // armo modelo único
+    constructor(id, nombre, talle, color, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.talle = talle;
+        this.color = color;
+        this.precio = precio;
+    }
 }
+//--------------Creo productos y los almaceno en un array-------------------------------------------//
+
+    const prenda1 = new Prenda(1, "Buzo ", "L", "marrón", 14000);
+    const prenda2 = new Prenda(2, "Buzo", "M", "gris", 16150);
+    const prenda3 = new Prenda(3, "Buzo", "X", "rosado", 11640);
+    const prenda4 = new Prenda(3, "Remera", "X", "blanca", 7050);
+
+
+    const prendas = [prenda1, prenda2, prenda3, prenda4];
+
+
+//--------------------- elemento del DOM que voy a necesitar------------------------------//
+
+const contenedorPrendas = document.getElementById('contenedorPrendas');
+
+
+//---------------- modifico el DOM --------------------------------------------------------//
+prendas.forEach((prenda) => {
+    const divPrenda = document.createElement('div');
+    divPrenda.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
+    divPrenda.innerHTML = `
+                            <div>
+                                <img src="img/${prenda.id}.jpg" class="card-img-top img-fluid py-3">
+                                <div class="card-body">
+                                    <h3 class="card-title"> ${prenda.nombre} </h3>
+                                    <p class="card-text"> ${prenda.precio} </p>
+                                    <button id="boton${prenda.id}" class="btn btn-primary"> Agregar al Carrito </button>
+                                </div>
+                            </div>`;
+    contenedorPrendas.appendChild(divPrenda);
+//--------------------------Agregar un evento al boton de añadir al carrito-------------------------------//
+const boton = document.getElementById(`boton${prenda.id}`);
+    boton.addEventListener('click', () => {
+    agregarAlCarrito(prenda.id);
+});
+});
+//-------------------------creo el carrito de compras ---------------------------------------------------//
+
+const carrito = [];
+
+//------------------creo una función que busque el producto por id y lo añada al carrito-----------------//
+const agregarAlCarrito = (id) => {
+    const prenda = prendas.find((prenda) => prenda.id === id);
+    const prendaEnCarrito = carrito.find((prenda) => prenda.id === id);
+    if (prendaEnCarrito) {
+        prendaEnCarrito.cantidad++;
+    } else {
+        carrito.push(prenda);
+    }
+    actualizarCarrito();
+};
+//-------------------muestro el carrito de compras modificando el DOM------------------------------------//
+
+const contenedorCarrito = document.getElementById('contenedorCarrito');
+const verCarrito = document.getElementById('verCarrito');
+
+verCarrito.addEventListener('click', actualizarCarrito);
+
+function actualizarCarrito() {
+let aux = '';
+carrito.forEach((prenda) => {
+    aux += `
+            <div class="card col-xl-3 col-md-6 col-sm-12">
+                <img src="img/${prenda.id}.jpg" class="card-img-top img-fluid py-3">
+                <div class="card-body">
+                    <h3 class="card-title"> ${prenda.nombre} </h3>
+                    <p class="card-text"> ${prenda.precio} </p>
+                    <button onClick = "eliminarDelCarrito(${prenda.id})" class="btn btn-primary"> Eliminar del Carrito </button>
+                </div>
+            </div>
+            `;
+});
+
+contenedorCarrito.innerHTML = aux;
+calcularTotalCompra();
+}
+//----------------------------agrego una función que elimine el producto del carrito----------------------//
+
+const eliminarDelCarrito = (id) => {
+    const prenda = carrito.find((prenda) => prenda.id === id);
+    carrito.splice(carrito.indexOf(prenda), 1);
+    actualizarCarrito();
+};
+ //--------------------------creo una función para vaciar todo el carrito por completo---------------------//
+
+const vaciarCarrito = document.getElementById('vaciarCarrito');
+vaciarCarrito.addEventListener('click', () => {
+carrito.splice(0, carrito.length);
+actualizarCarrito();
+});
+
+//----------------------------creo una función que me calcule el total del carrito------------------------//
+
+const totalCompra = document.getElementById('totalCompra');
+
+const calcularTotalCompra = () => {
+let total = 0;
+carrito.forEach((prenda) => {
+    total += prenda.precio * prenda.cantidad;
+});
+totalCompra.innerHTML = total;
+};
+
+
 
 //---------------------------guardar en el localstorage el DOM-------------------------------------------//
-btncarrito.addEventListener('click',()=>{
-    localStorage.setItem("buzo marron",btncarrito1.value);
-    localStorage.setItem("buzo gris",btncarrito2.value);
-    localStorage.setItem("buzo rosado",btncarrito3.value);
-    localStorage.setItem("remera blanca",btncarrito4.value);
+carrito.addEventListener('click',()=>{
+    localStorage.setItem(prenda1.value);
+    localStorage.setItem(prenda2.value);
+    localStorage.setItem(prenda3.value);
+    localStorage.setItem(prenda4.value);
 })
 //----------------------- cantidad de claves guardadas-------------------------------------------------//
-console.log(sessionStorage.length); 
-
-
-
-//------------cuando el usuario desee hacer click en el boton de carrito(conectar js con html)---------//
-
-document.getElementById("carrito1").addEventListener("click", hacerPregunta);
-document.getElementById("carrito2").addEventListener("click", hacerPregunta);
-document.getElementById("carrito3").addEventListener("click", hacerPregunta);
-document.getElementById("carrito4").addEventListener("click", hacerPregunta);
-let elegirProducto = 0;
-
-function hacerPregunta() {
-    //--------------------- pedir al usuario su nombre y preguntar si desea comprar ---------------------//
-    elegirProducto = ("¿Desea comprar?");
-    console.log(elegirProducto);
-
-}
-
-if (elegirProducto === "si" || elegirProducto === "Si") {
-
-    let producto = parseInt(prompt("Elige un producto, ingrese el número: \n1- Buzo marron. \n2 - Buzo gris. \n3 - Buzo rosado. \n4- Presiona N para abandonar."));
-    if(producto <= 3 || producto >= 1){
-        const productoElegido = prendas.find(p => p.id === producto);
-        carrito.push(productoElegido);
-
-    }
-    if (producto > 3 || producto < 1) {
-        console.log("ingrese un número válido");
-
-        //----- actualizar el numero de los productos-------------------//
-        for (let i = 0; i < producto.length; i++) {
-            producto[i].cantidad += 1;
-
-            console.log(producto) ;
-        }
-
-    } else {
-        
-
-        letprecioTotal = (productoElegido.precio * unidad);
-        alert("Su precio total es de ${precioTotal}");
-        console.log("Unidades: ${unidad}, Producto:${productoElegido.nombre}, Precio total: ${precioTotal}");
-
-    }
-    const continuarCompra = confirm("¿desea seguir comprando?");
-    if (continuarCompra) {
-
-        const preciosPrendas = prendas.map(el => el.precio);
-        console.log(preciosPrendas);
-
-    } else {
-        alert("Gracias por visitarnos");
-    }
-
-}
-localStorage.clear()
+console.log(carrito.length); 
 
 
 
