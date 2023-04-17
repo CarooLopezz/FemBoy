657934d214bb4d0fc2d0032cbc52e82995021c3d
Carolina Lopez <caroolinalopez4@gmail.com>
2023-04-17 14:36:10 -0300

    ..

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const $btnAgregar = document.getElementById("agregar");
 const $btnVerCarrito = document.querySelector("#verCarrito");
 const $btnEliminarCarrito = document.getElementById('eliminarCarrito');
+const $btnVaciarCarrito = document.getElementById("vaciarCarrito");
 const $btnComprar = document.getElementById("comprar");
 let carritoItems=[];
 
@@ -35,50 +36,88 @@ fetch('data.json')
     `;
     carrito.appendChild(carritoItems);
   }
-  //funcion para quitar un producto
+  
+// Cargar los datos del archivo JSON
+async function loadJSON() {
+  const response = await fetch('data.json');
+  const data = await response.json();
+  return data;
+}
 
-    function removeFromCarrito(prendaId) {
-        carritoItems = carritoItems.filter(item => item.id !== prendaId);
-        renderCarritoItems();
-      }
+// Función para generar la lista de productos
+async function renderProducts() {
+  const data = await loadJSON();
+  data.productos.forEach((prenda) => {
+    const li = document.createElement('li');
+    li.textContent = prenda.nombre;
+    prendaList.appendChild(li);
+  });
+}
 
-      function clearCarrito() {
-        carritoItems = [];
-        renderCarritoItems();
+// Función para eliminar un producto
+async function deleteProduct() {
+  const prendaName = document.getElementById("eliminarCarrito");
+  const data = await loadJSON();
+  const prendaIndex = data.prendas.findIndex((prenda) => prenda.nombre === prendaName);
+  if (prendaIndex !== -1) {
+    data.prendas.splice(prendaIndex, 1);
+    const response = await fetch('data.json', {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+    console.log(`El producto ${prenda.nombre} fue eliminado.`);
+    renderProducts();
+  } else {
+    console.log(`No se encontró el producto ${prenda.nombre}.`);
+  }
+}
 
-      function renderCarritoItems() {
-        carrito.innerHTML = '';
-        if (carritoItems.length === 0) {
-          carrito.innerHTML = '<p>Carrito vacío</p>';
-        } else {
-          carritoItems.forEach(prenda => {
-            const carritoItems = document.createElement('div');
-            carrito.innerHTML = `
-              <p>${prenda.nombre} - $${prenda.precio}</p>
-              <button data-id="${prenda.id}">Eliminar un producto</button>
-            `;
-            const removeButton = document.getElementById('eliminarCarrito');
-            removeButton.addEventListener('click', () => {
-              removeFromCarrito(prenda.id);
-            });
-            carrito.appendChild(carritoItems);
-          });
-        }
-      }
-     
-      $btnEliminarCarrito.addEventListener('click', () => {
-        clearCarrito();
-      });
+// Asignar evento de clic al botón de eliminar
+$btnEliminarCarrito.addEventListener('click', deleteProduct);
+
+// Generar la lista de productos al cargar la página
+renderProducts();
+//funcion para vaciar el producto
+
+function removeFromCarrito(prendaId) {
+  carritoItems = carritoItems.find(item => item.id == prendaId);
+  renderCarritoItems();
+
+}
+
+function clearCarrito() {
+  carritoItems = [];
+  renderCarritoItems();
+}
+
+function renderCarritoItems() {
+carrito.innerHTML = '';
+carritoItems.length === 0 {
+carrito.innerHTML = '<p>Carrito vacío</p>';
+  
+const removeButton = document.getElementById('eliminarCarrito');
+removeButton.addEventListener('click', () => {
+removeFromCarrito(prenda.id);
+});
+carrito.appendChild(carritoItems);
+};
+}
+
+$btnVaciarCarrito.addEventListener('click', () => {
+clearCarrito();
+});
    
-      // agregar al boton comprar un mensaje
-      function showConfirmationMessage(prenda) {
+// agregar al boton comprar un mensaje
+function showConfirmationMessage(prenda) {
         const message = `${prenda.nombre} se ha realizado la compra con exito.`;
         toastr.success(message);
-      }
-      $btnComprar.addEventListener("click",()=> {
+}
+$btnComprar.addEventListener("click",()=> {
         showConfirmationMessage();
-      })
+})
 
   
 
